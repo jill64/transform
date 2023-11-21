0158943eb31e1917e6fc2087feafbcc40e8e1b3e
jill64 <intents.turrets0h@icloud.com>
2023-11-21 21:25:35 +0900

    publish

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# transform
-Extended map functions for some JS objects
+<!----- BEGIN GHOST DOCS HEADER ----->
+
+# ts-library-template
+
+[![ci.yml](https://github.com/jill64/ts-library-template/actions/workflows/ci.yml/badge.svg)](https://github.com/jill64/ts-library-template/actions/workflows/ci.yml)
+
+⛩️ TypeScript Library Template
+
+## Install
+
+```sh
+npm i ts-library-template
+```
+
+<!----- END GHOST DOCS HEADER ----->
---
+++ index.test.ts
@@ -0,0 +1,39 @@
+import { expect, test } from 'vitest'
+import { transform } from './index.js'
+
+test('set', () => {
+  const res = transform(new Set([1, 2, 3] as const), (x) => x.toString())
+  expect(res).toEqual(new Set(['1', '2', '3']))
+})
+
+test('map', () => {
+  const res = transform(
+    new Map([
+      [1, 'a'],
+      [2, 'b'],
+      [3, 'c']
+    ] as const),
+    ([k, v], index) => [index, JSON.stringify([k, v])]
+  )
+
+  expect(res).toEqual(
+    new Map([
+      [0, '[1,"a"]'],
+      [1, '[2,"b"]'],
+      [2, '[3,"c"]']
+    ])
+  )
+})
+
+test('object', () => {
+  expect(
+    transform(
+      {
+        key: 'value'
+      } as const,
+      ([k, v]) => [k.toUpperCase(), v.toUpperCase()]
+    )
+  ).toEqual({
+    KEY: 'VALUE'
+  })
+})
