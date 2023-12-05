827591e99b3638600b97e7aa2830c094c9ce0c4d
jill64 <intents.turrets0h@icloud.com>
2023-12-05 15:39:12 +0900

    Update tsConfigPath in eslint.config.js

--- index.js
+++
@@ -1,6 +0,0 @@
-export const transform = (value, fn) =>
-  value instanceof Set
-    ? new Set([...value].map(fn))
-    : value instanceof Map
-      ? new Map([...value].map(fn))
-      : Object.fromEntries(Object.entries(value).map(fn))
